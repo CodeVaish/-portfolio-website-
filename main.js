d7a88f6a7c5a1065a5c7ff64f4524c33dd41ba73
CodeVaish <ayushishobhane@gmail.com>
2025-06-19 15:07:35 +0530

    files upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var type = new Typed(".text", {
+    strings: ["Frontend Developer", "Backend Developer", "Web Developer"], // Corrected strings array
+    typeSpeed: 100,   // Missing comma fixed
+    backSpeed: 100,   // Missing comma fixed
+    backDelay: 100,  // Missing comma fixed
+    loop: true        // Proper syntax
+});
